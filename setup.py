f8a650920f4d83143b9cc5a66ad264011a94a679
Daniel Cruise <danicdar@gmail.com>
2021-06-07 04:39:01 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='quillscripturesProject',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='danny',
+    author_email='danielfimo@live.com',
+    description='quillscriptures'
+)
