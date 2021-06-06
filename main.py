703ef2fd5944583a503de1241e1c3a41b6d12146
Daniel Cruz Dharma Anthuvan <danicdar@gmail.com>
2021-06-07 04:50:27 +0530

    pythonProject

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def welcome_to_quillscriptures(name):
+    # Use a breakpoint in the code line below to debug your script.
+    # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'welcome to quillscriptures {name}')
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    welcome_to_quillscriptures('Daniel Cruz')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
