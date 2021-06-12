a87d4806c5a20ae98908be02b343f459570027c6
Daniel Cruz Dharma Anthuvan <danicdar@gmail.com>
2021-06-12 20:01:05 +0530

    pythonProject

--- playground.swift
+++ playground.swift
@@ -1,3 +1,11 @@
 import Foundation
 
-print("Hello, World!")
+class BMI: ViewController {
+    private double bmiCalc = 0.0
+    public double weight = 0.0
+    public double height = 0.0
+    bmiCalc = (weight/height * height)
+    print("Your BMI is : \(bmiCalc)")
+}
+
+
